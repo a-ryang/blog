4bd923a292dc73cb5f054c328070d435c5d95eaf
a-ryang <chnwu.kim@gmail.com>
2023-06-27 23:21:05 +0900

    feat: 헤더 로고

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import Navbar from "./Navbar";
 
 const Header = () => {
@@ -6,7 +7,9 @@ const Header = () => {
     <header className="sticky top-0 backdrop-blur-md shadow-sm">
       <div className="flex justify-between items-center mx-auto px-4 xl:px-0 h-20 max-w-6xl">
         <h1 className="font-semibold text-lg">
-          <Link href="/">a-ryang</Link>
+          <Link href="/">
+            <Image src="/logo.png" width={40} height={40} alt="a-ryang"></Image>
+          </Link>
         </h1>
         <Navbar />
       </div>
