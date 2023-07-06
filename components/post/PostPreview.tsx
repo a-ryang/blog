6ab0a771b8104a4633b5c4ac91ac5dc080aee4a4
a-ryang <chnwu.kim@gmail.com>
2023-07-06 22:02:51 +0900

    feat: 썸네일

--- components/post/PostPreview.tsx
+++ components/post/PostPreview.tsx
@@ -9,24 +9,28 @@ type Items = {
 const PostPreview = ({ post }: { post: Items }) => {
   return (
     <div className="group w-full border-b border-neutral-200 dark:border-dark-100">
-      <Link href={`/blog/${post.slug}`} className="inline-block w-full py-10">
+      <Link href={`/blog/${post.slug}`} className="flex w-full py-10">
         {post?.thumbnail && (
-          <Image
-            alt={`cover image for ${post.title}`}
-            src={post.thumbnail}
-            width={400}
-            height={400}
-            style={{ width: "100%" }}
-            className="rounded-2xl"
-          />
+          <div className="relative rounded-md overflow-hidden mr-4">
+            <div className="absolute w-full h-full bg-gray-400 opacity-5"></div>
+            <Image
+              alt={`cover image for ${post.title}`}
+              src={post?.thumbnail}
+              width={128}
+              height={128}
+              className="h-full w-full object-cover"
+            />
+          </div>
         )}
-        <h3 className="font-semibold text-2xl group-hover:underline">
-          {post.title}
-        </h3>
-        <p className="mt-4 mb-2 md:mb-3.5 dark:text-neutral-300">
-          {post.excerpt}
-        </p>
-        <DateFormatter dateString={post.date} />
+        <div>
+          <h3 className="font-semibold text-2xl group-hover:underline">
+            {post.title}
+          </h3>
+          <p className="mt-4 mb-2 md:mb-3.5 dark:text-neutral-300">
+            {post.excerpt}
+          </p>
+          <DateFormatter dateString={post.date} />
+        </div>
       </Link>
     </div>
   );
