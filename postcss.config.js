3624c380cab4c29852ada8c01538ac4bbd07dc3f
a-ryang <chnwu.kim@gmail.com>
2023-07-07 10:11:58 +0900

    chore: fix typo

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 const purgecss = [
   "@fullhuman/postcss-purgecss",
   {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
+    content: ["./components/**/*.js", "./app/**/*.js"],
     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
 ];
