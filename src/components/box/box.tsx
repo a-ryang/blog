c511435e5c8032dcb266f3fe5b1bc760ab756174
a-ryang <chnwu.kim@gmail.com>
2023-10-12 03:13:55 +0900

    feat: Box component rest props

--- src/components/box/box.tsx
+++ src/components/box/box.tsx
@@ -3,14 +3,19 @@ import clsx from "clsx";
 import classes from "./box.module.css";
 import { StrictPropsWithChildren } from "@/types/utils";
 
-interface BoxProps {
+interface BoxProps extends React.ComponentProps<"div"> {
   style?: React.CSSProperties;
   className?: string;
 }
 
-export function Box({ style, className = "", children }: StrictPropsWithChildren<BoxProps>) {
+export function Box({
+  style,
+  className = "",
+  children,
+  ...props
+}: StrictPropsWithChildren<BoxProps>) {
   return (
-    <div style={style} className={clsx(classes.box, className)}>
+    <div style={style} className={clsx(classes.box, className)} {...props}>
       {children}
     </div>
   );
