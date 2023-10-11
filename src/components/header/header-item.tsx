c4a71b2d123ee0bb5540cdb63c9065fad974f1ea
a-ryang <chnwu.kim@gmail.com>
2023-10-12 01:58:41 +0900

    feat: header component

---
+++ src/components/header/header-item.tsx
@@ -0,0 +1,20 @@
+import { Icon } from "@phosphor-icons/react";
+import clsx from "clsx";
+import Link from "next/link";
+
+import classes from "./header-item.module.css";
+
+export interface HeaderItemProps {
+  icon: Icon;
+  label: string;
+  href: string;
+  isActive?: boolean;
+}
+
+export function HeaderItem({ icon: Icon, label, href, isActive }: HeaderItemProps) {
+  return (
+    <Link href={href} className={clsx(classes["header-item"], isActive ? classes.active : "")}>
+      <Icon size={20} weight="fill" />
+    </Link>
+  );
+}
