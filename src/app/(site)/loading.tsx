f9e511514a8c544b820245a5cbfdf437228a6f2c
a-ryang <chnwu.kim@gmail.com>
2023-10-12 01:58:57 +0900

    feat: loading page에 loader 반영

--- src/app/(site)/loading.tsx
+++ src/app/(site)/loading.tsx
@@ -1,3 +1,20 @@
+import { Deferrer } from "@/components/deferrer/deferrer";
+import { Loader } from "@/components/loader";
+
 export default function Loading() {
-  return <></>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        width: "100%",
+      }}
+    >
+      <Deferrer>
+        <Loader />
+      </Deferrer>
+    </div>
+  );
 }
