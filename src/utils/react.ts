0faece42e4fbfc10303f87c2dbc47173076ba5fc
a-ryang <chnwu.kim@gmail.com>
2024-02-04 11:44:17 +0900

    feat: isDev

--- src/utils/react.ts
+++ src/utils/react.ts
@@ -3,3 +3,5 @@ import { ReactNode } from "react";
 export type StrictPropsWithChildren<P = unknown> = P & {
   children: ReactNode;
 };
+
+export const isDev = process.env.NODE_ENV === "development";
